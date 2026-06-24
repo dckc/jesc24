946496459d859162a7ff9cadbae4326d304227bf
Dan Connolly <dckc@madmode.com>
2026-06-24 12:08:43 -0500

    feat: Jessie escrow to HLA (WIP)

---
+++ theories/jessie/escrow2013.v
@@ -0,0 +1,20 @@
+From Coq Require Import List String ZArith.
+From jessie Require Import escrow2013_js.
+From jessie Require Import escrow2013_target jessica_ast quasi_jessie.
+
+Import ListNotations.
+Open Scope string_scope.
+Open Scope Z_scope.
+
+Module Escrow2013.
+  Import Escrow2013Target.
+  Import JessicaAst.
+  Import QuasiJessie.
+
+  Definition parse_program_only (s : string) : option jmodule :=
+    QuasiJessie.parse_program_only s.
+
+  Example parse_escrow2013_source_program :
+    parse_program_only escrow2013_source = Some escrow2013_program.
+  Proof. vm_compute. reflexivity. Qed.
+End Escrow2013.
